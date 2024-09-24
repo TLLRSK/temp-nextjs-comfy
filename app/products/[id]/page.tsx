f6c898159d998e43f0ec9aa7bb8c118fe864b11a
TLLRSK <tullarisk@gmail.com>
2024-09-24 18:55:06 +0200

    first commit

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,50 @@
+import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
+import AddToCart from '@/components/single-product/AddToCart';
+import BreadCrumbs from '@/components/single-product/BreadCrumbs';
+import ProductRating from '@/components/single-product/ProductRating';
+import { fetchSingleProduct } from '@/utils/actions'
+import { formatCurrency } from '@/utils/format';
+import Image from 'next/image';
+import React from 'react'
+
+async function SingleProductPage({params} : {params: {id: string}}) {
+  const product = await fetchSingleProduct(params.id);
+  console.log(product)
+  const { name, image, company, description, price } = product;
+  const dollarsAmount = formatCurrency(price);
+
+  return (
+    <section>
+        <BreadCrumbs name={product.name} />
+        <div className='mt-6 grid gap-y8 lg:grid-cols-2 lg:gap-x-16'>
+          <div className='relative h-full'>
+            <Image
+              src={image}
+              alt={name}
+              fill
+              sizes='(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw'
+              priority
+              className='w-full rounded object-cover'
+            />
+          </div>
+          <div>
+            <div className='flex gap-x-8 items-center'>
+              <h1 className='capitalize text-3xl font-bold'>{name} </h1>
+              <div className='flex items-center gap-x-2'>
+                <FavoriteToggleButton productId={params.id} />
+              </div>
+            </div>
+            <ProductRating productId={params.id} />
+            <h4 className='text-xl mt-2'>{company}</h4>
+            <p className='mt-3 text-md bg-muted inline-block p-2 rounded'>
+              {dollarsAmount}
+            </p>
+            <p className='mt-6 leading-8 text-muted-foreground'>{description}</p>
+            <AddToCart productId={params.id} />
+          </div>
+        </div>
+    </section>
+  )
+}
+
+export default SingleProductPage
---
+++ utils/actions.ts
@@ -0,0 +1,38 @@
+import db from '@/utils/db';
+
+export const fetchFeaturedProducts = async() => {
+    const products = await db.product.findMany({
+        where: {
+            featured: true,
+        },
+        // select: {
+        //     name: true,
+        // }
+    });
+    return products;
+}
+
+export const fetchAllProducts = ({ search = '' }: {search: string}) => {
+    const products = db.product.findMany({
+        where: {
+            OR: [
+                {name: {contains: search, mode: 'insensitive'}},
+                {company: {contains: search, mode: 'insensitive'}},
+            ]
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+    });
+    return products;
+}
+
+export const fetchSingleProduct = async( productId: string) => {
+    const product = await db.product.findUnique({
+        where: {
+            id: productId,
+        },
+    });
+    if (!product) redirect('/products');
+    return product;
+}
