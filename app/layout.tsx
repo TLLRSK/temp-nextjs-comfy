cbf7963ca7a03317e286c006c3c9d4cbac6db260
TLLRSK <tullarisk@gmail.com>
2024-09-28 12:30:02 +0200

    add auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Container from "@/components/global/Container";
 import Navbar from "@/components/navbar/Navbar";
 import Providers from "./providers";
+import { ClerkProvider } from '@clerk/nextjs';
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,17 +29,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>
-          <Navbar/>
-          <Container className="py-20">
-            {children}
-          </Container>
-        </Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Providers>
+            <Navbar/>
+            <Container className="py-20">
+              {children}
+            </Container>
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+'use client'
+
+import { useToast } from "@/hooks/use-toast";
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 function SignOutLink() {
+  const { toast } = useToast();
+  const handleLogOut = () => {
+    toast({description: 'Logout Success'});
+  }
   return (
-    <div>SignOutLink</div>
+    <SignOutButton>
+      <Link href='/' className="w-full text-left" onClick={handleLogOut}>
+        Logout
+      </Link>
+    </SignOutButton>
   )
 }
 
