fe75c3729b14b35307853583fa7c231edf51c3ec
TLLRSK <tullarisk@gmail.com>
2024-09-30 13:26:51 +0200

    edit product

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,4 +1,8 @@
-import { fetchAdminProductDetails, updateProductAction, updateProductImageAction } from '@/utils/actions';
+import {
+  fetchAdminProductDetails,
+  updateProductAction,
+  updateProductImageAction,
+} from '@/utils/actions';
 import FormContainer from '@/components/form/FormContainer';
 import FormInput from '@/components/form/FormInput';
 import PriceInput from '@/components/form/PriceInput';
@@ -14,31 +18,26 @@ async function EditProductPage({ params }: { params: { id: string } }) {
   return (
     <section>
       <h1 className='text-2xl font-semibold mb-8 capitalize'>update product</h1>
-      <div className='border p-8 rounded-md'>
-        <ImageInputContainer 
-          action={updateProductImageAction} 
-          name={name} 
-          image={product.image} 
-          text='Update Image'
+      <div className='border p-8 rounded'>
+        <ImageInputContainer
+          action={updateProductImageAction}
+          name={name}
+          image={product.image}
+          text='update image'
         >
           <input type='hidden' name='id' value={id} />
-          <input type="hidden" name='url' value={product.image} />
+          <input type='hidden' name='url' value={product.image} />
         </ImageInputContainer>
         <FormContainer action={updateProductAction}>
           <div className='grid gap-4 md:grid-cols-2 my-4'>
+            <input type='hidden' name='id' value={id} />
             <FormInput
               type='text'
               name='name'
               label='product name'
               defaultValue={name}
             />
-            <FormInput
-              type='text'
-              name='company'
-              label='company'
-              defaultValue={company}
-            />
-
+            <FormInput type='text' name='company' defaultValue={company} />
             <PriceInput defaultValue={price} />
           </div>
           <TextAreaInput
@@ -53,7 +52,7 @@ async function EditProductPage({ params }: { params: { id: string } }) {
               defaultChecked={featured}
             />
           </div>
-          <SubmitButton text='Update Product' className='mt-8' />
+          <SubmitButton text='update product' className='mt-8' />
         </FormContainer>
       </div>
     </section>
