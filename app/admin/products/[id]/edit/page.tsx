0f4314460615b66e8a8e6449f061c3436cb8f4e0
TLLRSK <tullarisk@gmail.com>
2024-09-30 13:57:48 +0200

    comit

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,33 +1,38 @@
+import { SubmitButton } from '@/components/form/Buttons';
+import CheckboxInput from '@/components/form/CheckboxInput';
+import FormContainer from '@/components/form/FormContainer';
+import FormInput from '@/components/form/FormInput';
+import ImageInput from '@/components/form/ImageInput';
+import PriceInput from '@/components/form/PriceInput';
+import TextAreaInput from '@/components/form/TextAreaInput';
+import { Button } from '@/components/ui/button';
+import { createProductAction } from '@/utils/actions';
+import { faker } from '@faker-js/faker';
 
-async function EditProductPage({ params }: { params: { id: string } }) {
-  const { id } = params;
-  // const product = await fetchAdminProductDetails(id);
-  // const { name, company, description, featured, price } = product;
+function CreateProductPage() {
+  const name = faker.commerce.productName();
+  const company = faker.company.name();
+  const description = faker.lorem.paragraph({ min: 10, max: 12 });
   return (
     <section>
-      <h2>{id}</h2>
-      {/* <h1 className='text-2xl font-semibold mb-8 capitalize'>update product</h1>
-      <div className='border p-8 rounded'>
-        <ImageInputContainer
-          action={updateProductImageAction}
-          name={name}
-          image={product.image}
-          text='update image'
-        >
-          <input type='hidden' name='id' value={id} />
-          <input type='hidden' name='url' value={product.image} />
-        </ImageInputContainer>
-        <FormContainer action={updateProductAction}>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>create product</h1>
+      <div className='border p-8 rounded-md'>
+        <FormContainer action={createProductAction}>
           <div className='grid gap-4 md:grid-cols-2 my-4'>
-            <input type='hidden' name='id' value={id} />
             <FormInput
               type='text'
               name='name'
               label='product name'
               defaultValue={name}
             />
-            <FormInput type='text' name='company' defaultValue={company} />
-            <PriceInput defaultValue={price} />
+            <FormInput
+              type='text'
+              name='company'
+              label='company'
+              defaultValue={company}
+            />
+            <PriceInput />
+            <ImageInput />
           </div>
           <TextAreaInput
             name='description'
@@ -35,16 +40,12 @@ async function EditProductPage({ params }: { params: { id: string } }) {
             defaultValue={description}
           />
           <div className='mt-6'>
-            <CheckboxInput
-              name='featured'
-              label='featured'
-              defaultChecked={featured}
-            />
+            <CheckboxInput name='featured' label='featured' />
           </div>
-          <SubmitButton text='update product' className='mt-8' />
+          <SubmitButton text='create product' className='mt-8' />
         </FormContainer>
-      </div> */}
+      </div>
     </section>
   );
 }
-export default EditProductPage;
+export default CreateProductPage;
