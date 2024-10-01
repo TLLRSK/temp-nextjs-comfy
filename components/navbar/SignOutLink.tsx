860e9578a707cb2da223cebd66a593fb5621e9f3
TLLRSK <tullarisk@gmail.com>
2024-10-01 16:45:27 +0200

    add stripe

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,21 +1,19 @@
-'use client'
+'use client';
 
-import { useToast } from "@/hooks/use-toast";
-import { SignOutButton } from "@clerk/nextjs";
-import Link from "next/link";
-
+import { useToast } from '@/hooks/use-toast';
+import { SignOutButton } from '@clerk/nextjs';
+import Link from 'next/link';
 function SignOutLink() {
   const { toast } = useToast();
-  const handleLogOut = () => {
-    toast({description: 'Logout Success'});
-  }
+  const handleLogout = () => {
+    toast({ description: 'Logout Successful' });
+  };
   return (
     <SignOutButton>
-      <Link href='/' className="w-full text-left" onClick={handleLogOut}>
+      <Link href='/' className='w-full text-left' onClick={handleLogout}>
         Logout
       </Link>
     </SignOutButton>
-  )
+  );
 }
-
-export default SignOutLink
+export default SignOutLink;
